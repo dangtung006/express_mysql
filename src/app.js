2cb7b70a4c2ad263e58ec2ff745e19788b6e5558
dangtung <language0512@gmail.com>
2023-05-13 13:52:24 +0700

    update app middlewares

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const appRoutes = require("./routes/index");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(appRoutes());
 const server = app.listen(3001, () => {
     console.log("app listen on port 3001")
